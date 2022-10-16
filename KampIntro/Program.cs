426e73b1da83726f9665704fb4ad7b7b64ee0afa
salihyksl <salihyksl42@gmail.com>
2022-10-16 11:04:53 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirşYapmisMi = false;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butıonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            
+            if (sistemeGirşYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarlar Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+            
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
